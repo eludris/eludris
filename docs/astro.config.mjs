aa24b65d8c78def76ca278b8bcccb8b3c8f71b44
Enoki <enokiun@gmail.com>
2023-04-10 23:17:20 +0300

    Revert "docs: better cloudflare support"
    This reverts commit ed6f123c95756bf6db3e9c7bdfe4c1f015acc91a.

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+
 import { readFileSync, writeFileSync } from 'fs';
 import sitemap from '@astrojs/sitemap';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
@@ -15,37 +16,31 @@ import { visit } from 'unist-util-visit';
 import { toString } from 'mdast-util-to-string';
 
 // we have to do this entire loop because code nodes are their own things so stuff like mdast-util-find-and-replace won't work
-import cloudflare from "@astrojs/cloudflare";
 const remarkAutolinkReferenceEntries = () => {
-  return tree => {
-    const text = toMarkdown(tree, {
-      extensions: [gfmTableToMarkdown()]
-    });
-    return fromMarkdown(text.replace(/\\\[`(.+)`\]/gm, (_, p1) => {
-      const item = AUTODOC_ENTRIES.find(entry => entry.endsWith(`/${p1}.json`));
-      if (!item) {}
-      return `[${p1.replace(/(?:^|_)([a-z0-9])/gm, (_, p1) => p1.toUpperCase()).replace(/[A-Z]/gm, ' $&').trim()}](/reference/${item.split('.')[0]})`;
-    }), {
-      extensions: [gfmTable],
-      mdastExtensions: [gfmTableFromMarkdown]
-    });
+  return (tree) => {
+    const text = toMarkdown(tree, { extensions: [gfmTableToMarkdown()] });
+    return fromMarkdown(
+      text.replace(/\\\[`(.+)`\]/gm, (_, p1) => {
+        const item = AUTODOC_ENTRIES.find((entry) => entry.endsWith(`/${p1}.json`));
+        if (!item) {
+        }
+        return `[${p1
+          .replace(/(?:^|_)([a-z0-9])/gm, (_, p1) => p1.toUpperCase())
+          .replace(/[A-Z]/gm, ' $&')
+          .trim()}](/reference/${item.split('.')[0]})`;
+      }),
+      { extensions: [gfmTable], mdastExtensions: [gfmTableFromMarkdown] }
+    );
   };
 };
+
 const remarkGenerateSearchIndex = () => {
   return (tree, file) => {
-    let sections = [{
-      line: 0,
-      content: ''
-    }];
-    visit(tree, 'heading', node => {
+    let sections = [{ line: 0, content: '' }];
+    visit(tree, 'heading', (node) => {
       let text = toMarkdown(node).trim().replace(/^#+ /, '');
       let id = toString(node).trim().toLowerCase().replace(/ /gm, '-');
-      sections.push({
-        line: node.position.start.line,
-        text,
-        id,
-        content: ''
-      });
+      sections.push({ line: node.position.start.line, text, id, content: '' });
     });
     sections = sections.sort((a, b) => b.line - a.line);
     let baseRoute;
@@ -57,10 +52,19 @@ const remarkGenerateSearchIndex = () => {
       }
     } else {
       // probably an autodoc entry
-      const entry = AUTODOC_ENTRIES.find(e => sections[sections.length - 2].text == e.split('/')[1].split('.')[0].replace(/(?:^|_)([a-z0-9])/gm, (_, p1) => p1.toUpperCase()).replace(/[A-Z]/gm, ' $&').trim()).split('.')[0];
+      const entry = AUTODOC_ENTRIES.find(
+        (e) =>
+          sections[sections.length - 2].text ==
+          e
+            .split('/')[1]
+            .split('.')[0]
+            .replace(/(?:^|_)([a-z0-9])/gm, (_, p1) => p1.toUpperCase())
+            .replace(/[A-Z]/gm, ' $&')
+            .trim()
+      ).split('.')[0];
       baseRoute = `/reference/${entry}`;
     }
-    visit(tree, 'paragraph', node => {
+    visit(tree, 'paragraph', (node) => {
       for (let i = 0; i < sections.length; i++) {
         if (sections[i].line < node.position.start.line) {
           sections[i].content += toString(node).replace(/\n/gm, ' ');
@@ -69,7 +73,7 @@ const remarkGenerateSearchIndex = () => {
       }
       sections[sections.length - 1].content += toString(node).replace(/\n/gm, ' ');
     });
-    visit(tree, 'tableCell', node => {
+    visit(tree, 'tableCell', (node) => {
       for (let i = 0; i < sections.length; i++) {
         if (sections[i].line < node.position.start.line) {
           sections[i].content += ' ' + toString(node).replace(/\n/gm, ' ');
@@ -78,15 +82,13 @@ const remarkGenerateSearchIndex = () => {
       }
       sections[sections.length - 1].content += ' ' + toString(node).replace(/\n/gm, ' ');
     });
-    sections = sections.filter(s => s.text).map(s => ({
-      route: `${baseRoute}${s.id ? '#' + s.id : ''} `,
-      ...s
-    }));
+    sections = sections
+      .filter((s) => s.text)
+      .map((s) => ({ route: `${baseRoute}${s.id ? '#' + s.id : ''} `, ...s }));
     const data = JSON.parse(readFileSync('public/search.json'));
     writeFileSync('public/search.json', JSON.stringify([...data, ...sections.reverse()]));
   };
 };
-
 
 // https://astro.build/config
 export default defineConfig({
@@ -100,16 +102,21 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: 'prism',
     remarkPlugins: [remarkAutolinkReferenceEntries, remarkGenerateSearchIndex],
-    rehypePlugins: [rehypeAccessibleEmojis, rehypeSlug, [rehypeAutolinkHeadings, {
-      behavior: 'before',
-      content() {
-        return h('span.header-icon', '>>');
-      },
-      group() {
-        return h('span.header');
-      }
-    }]]
-  },
-  output: "server",
-  adapter: cloudflare()
+    rehypePlugins: [
+      rehypeAccessibleEmojis,
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'before',
+          content() {
+            return h('span.header-icon', '>>');
+          },
+          group() {
+            return h('span.header');
+          }
+        }
+      ]
+    ]
+  }
 });
