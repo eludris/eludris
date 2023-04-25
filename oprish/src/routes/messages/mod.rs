302c11e01ac513c9e77733995a5f5c9e0eba02eb
Enoki <enokiun@gmail.com>
2023-04-25 22:03:49 +0300

    feat(oprish): update message create route to work with new errors

--- oprish/src/routes/messages/mod.rs
+++ oprish/src/routes/messages/mod.rs
@@ -5,7 +5,7 @@ use rocket::serde::json::Json;
 use rocket::{Route, State};
 use rocket_db_pools::Connection;
 use todel::http::ClientIP;
-use todel::models::{ErrorResponse, ErrorResponseData, Message, ServerPayload, ValidationError};
+use todel::models::{ErrorResponse, Message, ServerPayload};
 use todel::Conf;
 
 #[autodoc("/messages", category = "Messaging")]
@@ -18,33 +18,34 @@ pub async fn create_message(
 ) -> RateLimitedRouteResponse<Result<Json<Message>, ErrorResponse>> {
     let mut rate_limiter = RateLimiter::new("message_create", address, conf.inner());
     rate_limiter.process_rate_limit(&mut cache).await?;
+
     let message = message.into_inner();
     if message.author.len() < 2 || message.author.len() > 32 {
-        rate_limiter.wrap_response(Err(ValidationError {
-            field_name: "author".to_string(),
-            error: "Message author has to be between 2 and 32 characters long.".to_string(),
-        }
-        .to_error_response()))
+        error!(
+            rate_limiter,
+            VALIDATION, "author", "Message author has to be between 2 and 32 characters long"
+        );
     } else if message.content.is_empty() || message.content.len() > conf.oprish.message_limit {
-        rate_limiter.wrap_response(Err(ValidationError {
-            field_name: "content".to_string(),
-            error: format!(
-                "Message content has to be between 1 and {} characters long.",
+        error!(
+            rate_limiter,
+            VALIDATION,
+            "content",
+            format!(
+                "Message content has to be between 1 and {} characters long",
                 conf.oprish.message_limit
-            ),
-        }
-        .to_error_response()))
+            )
+        );
+    }
+
+    let payload = ServerPayload::MessageCreate(message);
+    cache
+        .publish::<&str, String, ()>("oprish-events", serde_json::to_string(&payload).unwrap())
+        .await
+        .unwrap();
+    if let ServerPayload::MessageCreate(message) = payload {
+        rate_limiter.wrap_response(Ok(Json(message)))
     } else {
-        let payload = ServerPayload::MessageCreate(message);
-        cache
-            .publish::<&str, String, ()>("oprish-events", serde_json::to_string(&payload).unwrap())
-            .await
-            .unwrap();
-        if let ServerPayload::MessageCreate(message) = payload {
-            rate_limiter.wrap_response(Ok(Json(message)))
-        } else {
-            unreachable!()
-        }
+        unreachable!()
     }
 }
 
