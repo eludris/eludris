672b08c5974d583113af57b96c9289dbbf46f658
Enoki <enokiun@gmail.com>
2023-04-29 20:00:45 +0300

    docs(todel): properly document the `Message` model

--- todel/src/models/messages.rs
+++ todel/src/models/messages.rs
@@ -5,8 +5,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Message {
     /// The author of message. This field has to be between 2 and 32 characters long.
+    ///
+    /// The author will be trimmed from leading and trailing whitespace.
     pub author: String,
     /// The content of the message. This field has to be at-least 2 characters long. The upper limit
     /// is the instance's [`InstanceInfo`] `message_limit`.
+    ///
+    /// You cannot send messages which are just whitespace.
     pub content: String,
 }
