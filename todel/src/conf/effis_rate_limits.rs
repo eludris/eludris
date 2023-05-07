ff1e6bf28c17eed64b4567e25767536942b74554
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-05-08 00:55:17 +0100

    docs(todel): document EffisRateLimits

--- todel/src/conf/effis_rate_limits.rs
+++ todel/src/conf/effis_rate_limits.rs
@@ -3,13 +3,40 @@ use ubyte::ByteUnit;
 
 use super::RateLimitConf;
 
-/// Effis rate limit data config.
+/// Rate limits that apply to Effis (The CDN).
+///
+/// -----
+///
+/// # Example
+///
+/// ```json
+/// {
+///   "assets": {
+///     "reset_after": 60,
+///     "limit": 5,
+///     "file_size_limit": 30000000
+///   },
+///   "attachments": {
+///     "reset_after": 180,
+///     "limit": 20,
+///     "file_size_limit": 500000000
+///   },
+///   "fetch_file": {
+///     "reset_after": 60,
+///     "limit": 30
+///   }
+/// }
+/// ```
+#[autodoc(category = "Instance")]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct EffisRateLimits {
+    /// Rate limits for the asset buckets.
     #[serde(default = "assets_default")]
     pub assets: EffisRateLimitConf,
+    /// Rate limits for the attachment bucket.
     #[serde(default = "attachments_default")]
     pub attachments: EffisRateLimitConf,
+    /// Rate limits for the file fetching endpoints.
     #[serde(default = "fetch_file_default")]
     pub fetch_file: RateLimitConf,
 }
