c247a3ac3989d1a2b276ab1bd4e46978b6d6602f
Enoki <enokiun@gmail.com>
2023-05-18 10:37:37 +0300

    test: make tests work... again

--- effis/src/main.rs
+++ effis/src/main.rs
@@ -9,7 +9,7 @@ mod routes;
 
 #[cfg(test)]
 use std::sync::Once;
-use std::{env, path::Path};
+use std::{env, fs, path::Path};
 
 use anyhow::Context;
 
@@ -20,7 +20,6 @@ use rocket::{
 };
 use rocket_db_pools::{deadpool_redis::Pool, sqlx::MySqlPool, Database};
 use todel::{ids::IDGenerator, Conf};
-use tokio::fs;
 
 pub const BUCKETS: [&str; 1] = ["attachments"];
 
@@ -41,6 +40,7 @@ fn rocket() -> Result<Rocket<Build>, anyhow::Error> {
         INIT.call_once(|| {
             env::set_current_dir("..").expect("Could not set the current directory");
             env::set_var("ELUDRIS_CONF", "tests/Eludris.toml");
+            create_file_dirs().expect("Could not create necessary file directories");
             dotenvy::dotenv().ok();
             env_logger::init();
         });
@@ -114,7 +114,7 @@ async fn main() -> Result<(), anyhow::Error> {
         .await
         .context("Failed to run migrations")?;
 
-    create_file_dirs().await?;
+    create_file_dirs()?;
 
     let _ = rocket()?
         .launch()
@@ -124,19 +124,18 @@ async fn main() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-async fn create_file_dirs() -> Result<(), anyhow::Error> {
-    try_create_dir("files").await?;
-    try_create_dir("files/static").await?;
+fn create_file_dirs() -> Result<(), anyhow::Error> {
+    try_create_dir("files")?;
+    try_create_dir("files/static")?;
     for dir in BUCKETS.iter() {
-        try_create_dir(format!("files/{dir}")).await?;
+        try_create_dir(format!("files/{dir}"))?;
     }
     Ok(())
 }
 
-async fn try_create_dir(path: impl AsRef<Path>) -> Result<(), anyhow::Error> {
+fn try_create_dir(path: impl AsRef<Path>) -> Result<(), anyhow::Error> {
     if !path.as_ref().exists() {
         fs::create_dir(&path)
-            .await
             .with_context(|| format!("Failed to create {} directory", path.as_ref().display()))
     } else {
         Ok(())
