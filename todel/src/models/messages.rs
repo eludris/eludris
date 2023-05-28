2a1e0766e8167f68c47b8e4862a001f8c9687559
ceilia <ceilialionhrat@gmail.com>
2023-05-28 15:57:06 +0300

    docs(todel): make the documentation of `Message`'s field more consistent with other models

--- todel/src/models/messages.rs
+++ todel/src/models/messages.rs
@@ -15,11 +15,11 @@ use serde::{Deserialize, Serialize};
 #[autodoc(category = "Messaging")]
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Message {
-    /// The author of message. This field has to be between 2 and 32 characters long.
+    /// The message's author. This field has to be between 2 and 32 characters long.
     ///
     /// The author will be trimmed from leading and trailing whitespace.
     pub author: String,
-    /// The content of the message. This field has to be at-least 2 characters long. The upper limit
+    /// The message's content the message. This field has to be at-least 2 characters long. The upper limit
     /// is the instance's [`InstanceInfo`] `message_limit`.
     ///
     /// The content will be trimmed from leading and trailing whitespace.
