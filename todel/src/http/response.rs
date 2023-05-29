cb090957e95bd55ea7231d9a5439f849a13da0a7
Ceilia <134708916+ceiliaa@users.noreply.github.com>
2023-05-29 20:30:05 +0300

    feat(todel): add `Unauthorized` and `Foridden` to `ErrorResponse` (#90)
    * feat(todel): add `Unauthorized` and `Forbidden` to `ErrorResponse`
    
    * docs(todel): remove extra dash in the `ErrorResponse::RateLimited` variant docs

--- todel/src/http/response.rs
+++ todel/src/http/response.rs
@@ -10,9 +10,11 @@ use crate::models::ErrorResponse;
 impl<'r> Responder<'r, 'static> for ErrorResponse {
     fn respond_to(self, req: &'r Request<'_>) -> response::Result<'static> {
         let status = match &self {
-            ErrorResponse::RateLimited { shared, .. } => shared.status,
-            ErrorResponse::Validation { shared, .. } => shared.status,
+            ErrorResponse::Unauthorized { shared, .. } => shared.status,
+            ErrorResponse::Forbidden { shared, .. } => shared.status,
             ErrorResponse::NotFound { shared, .. } => shared.status,
+            ErrorResponse::Validation { shared, .. } => shared.status,
+            ErrorResponse::RateLimited { shared, .. } => shared.status,
             ErrorResponse::Server { shared, .. } => shared.status,
         };
         response::Response::build_from(Json(self).respond_to(req)?)
