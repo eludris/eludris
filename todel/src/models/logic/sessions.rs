01f1e29a3acf3895456100f1b5c35860e79c9873
ceilia <ceilialionhrat@gmail.com>
2023-05-29 20:09:01 +0300

    feat(todel): add the `SessionTokenClaims` struct for JWT payloads

--- todel/src/models/logic/sessions.rs
+++ todel/src/models/logic/sessions.rs
@@ -1,4 +1,10 @@
-use crate::models::SessionCreate;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct SessionTokenClaims {
+    user_id: u64,
+    session_id: u64,
+}
 
 impl SessionCreate {
     pub fn ensure_valid(&mut self) {
