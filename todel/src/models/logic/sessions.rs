2d756ff3a58df20e5bef42d8050c5295b26955fe
ceilia <ceilialionhrat@gmail.com>
2023-05-30 20:23:14 +0300

    feat(todel): implement `Session::create` to allow for token creation

--- todel/src/models/logic/sessions.rs
+++ todel/src/models/logic/sessions.rs
@@ -1,4 +1,16 @@
+use std::net::IpAddr;
+
+use argon2::{PasswordHash, PasswordVerifier};
+use jwt::SignWithKey;
 use serde::{Deserialize, Serialize};
+use sqlx::{pool::PoolConnection, types::ipnetwork::IpNetwork, Postgres};
+
+use crate::{
+    ids::IdGenerator,
+    models::{ErrorResponse, Session, SessionCreate, SessionCreated},
+};
+
+use super::Secret;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct SessionTokenClaims {
@@ -10,5 +22,79 @@ impl SessionCreate {
     pub fn ensure_valid(&mut self) {
         self.platform = self.platform.to_lowercase();
         self.client = self.client.to_lowercase();
+    }
+}
+
+impl Session {
+    pub async fn create<V: PasswordVerifier>(
+        mut session: SessionCreate,
+        ip: IpAddr,
+        secret: &Secret,
+        verifier: &V,
+        id_generator: &mut IdGenerator,
+        db: &mut PoolConnection<Postgres>,
+    ) -> Result<SessionCreated, ErrorResponse> {
+        session.ensure_valid();
+        let user = sqlx::query!(
+            "
+SELECT id, password
+FROM users
+WHERE username = $1
+OR email = $1
+            ",
+            session.identifier
+        )
+        .fetch_optional(&mut *db)
+        .await
+        .map_err(|err| {
+            log::error!("Could not fetch the user's password: {}", err);
+            error!(SERVER, "Failed to fetch the user's password")
+        })?
+        .ok_or_else(|| error!(NOT_FOUND))?;
+        verifier
+            .verify_password(
+                session.password.as_bytes(),
+                &PasswordHash::new(&user.password).map_err(|err| {
+                    log::error!("Couldn't parse password hash: {}", err);
+                    error!(SERVER, "Failed to validate the user's password")
+                })?,
+            )
+            .map_err(|_| error!(UNAUTHORIZED))?;
+        let id = id_generator.generate();
+        sqlx::query!(
+            "
+INSERT INTO sessions(id, user_id, platform, client, ip)
+VALUES($1, $2, $3, $4, $5)
+            ",
+            id as i64,
+            user.id as i64,
+            session.platform,
+            session.client,
+            IpNetwork::from(ip),
+        )
+        .execute(db)
+        .await
+        .map_err(|err| {
+            log::error!("Failed to store session in database: {}", err);
+            error!(SERVER, "Could not save session data")
+        })?;
+        let claims = SessionTokenClaims {
+            user_id: user.id as u64,
+            session_id: id,
+        };
+        let token = claims.sign_with_key(&secret.0).map_err(|err| {
+            log::error!("Couldn't sign JWT: {}", err);
+            error!(SERVER, "Failed to generate a token for the user")
+        })?;
+        Ok(SessionCreated {
+            token,
+            session: Session {
+                id,
+                user_id: user.id as u64,
+                platform: session.platform,
+                client: session.client,
+                ip,
+            },
+        })
     }
 }
