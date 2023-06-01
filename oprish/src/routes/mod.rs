bd9d4d3dfeda12dbbc7c8995db32a9b5d276e3e7
Enoki <enokiun@gmail.com>
2023-06-02 01:44:31 +0300

    feat(oprish): add base user routes

--- oprish/src/routes/mod.rs
+++ oprish/src/routes/mod.rs
@@ -1,8 +1,19 @@
 pub mod messages;
 
+use argon2::Argon2;
+use rand::rngs::StdRng;
 use rocket::{serde::json::Json, Route, State};
 use rocket_db_pools::Connection;
-use todel::{http::ClientIP, models::InstanceInfo, Conf};
+use todel::{
+    http::{ClientIP, TokenAuth, DB},
+    ids::IdGenerator,
+    models::{
+        Emailer, ErrorResponse, InstanceInfo, Secret, Session, SessionCreate, SessionCreated, User,
+        UserCreate,
+    },
+    Conf,
+};
+use tokio::sync::Mutex;
 
 use crate::{
     rate_limit::{RateLimitedRouteResponse, RateLimiter},
@@ -78,8 +89,67 @@ pub async fn get_instance_info(
     rate_limiter.wrap_response(Json(InstanceInfo::from_conf(conf.inner(), rate_limits)))
 }
 
+#[post("/signup", data = "<user>")]
+pub async fn signup(
+    user: Json<UserCreate>,
+    hasher: &State<Argon2<'static>>,
+    rng: &State<Mutex<StdRng>>,
+    id_generator: &State<Mutex<IdGenerator>>,
+    conf: &State<Conf>,
+    mailer: &State<Emailer>,
+    mut db: Connection<DB>,
+    cache: Connection<Cache>,
+) -> Result<Json<User>, ErrorResponse> {
+    Ok(Json(
+        User::create(
+            user.into_inner(),
+            hasher.inner(),
+            &mut *rng.lock().await,
+            &mut *id_generator.lock().await,
+            conf,
+            mailer,
+            &mut db,
+            &mut cache.into_inner(),
+        )
+        .await?,
+    ))
+}
+
+#[post("/login", data = "<session>")]
+async fn login(
+    session: Json<SessionCreate>,
+    verifier: &State<Argon2<'static>>,
+    id_generator: &State<Mutex<IdGenerator>>,
+    secret: &State<Secret>,
+    mut db: Connection<DB>,
+    ip: ClientIP,
+) -> Result<Json<SessionCreated>, ErrorResponse> {
+    Ok(Json(
+        Session::create(
+            session.into_inner(),
+            *ip,
+            secret,
+            verifier.inner(),
+            &mut *id_generator.lock().await,
+            &mut db,
+        )
+        .await?,
+    ))
+}
+
+#[post("/verify?<code>")]
+async fn verify(
+    code: u32,
+    mut db: Connection<DB>,
+    cache: Connection<Cache>,
+    session: TokenAuth,
+) -> Result<(), ErrorResponse> {
+    User::verify(code, session.0, &mut db, &mut cache.into_inner()).await?;
+    Ok(())
+}
+
 pub fn get_routes() -> Vec<Route> {
-    routes![get_instance_info]
+    routes![get_instance_info, signup, login, verify]
 }
 
 #[cfg(test)]
