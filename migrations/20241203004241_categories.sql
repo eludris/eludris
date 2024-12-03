974c71c08c7b3621945ed20212cfacc2f56458ee
Chromosomologist <chromosomologist@gmail.com>
2024-12-03 02:15:02 +0100

    feat: idk sql migraine

---
+++ migrations/20241203004241_categories.sql
@@ -0,0 +1,21 @@
+-- welcom 2 penis databas
+--                                                                      cock
+
+ALTER TYPE channel_type RENAME TO channel_type_old;
+CREATE TYPE channel_type AS ENUM ('TEXT', 'VOICE', 'GROUP', 'DIRECT');
+
+ALTER TABLE channels ALTER COLUMN channel_type SET DEFAULT NULL;
+ALTER TABLE channels ALTER COLUMN channel_type TYPE channel_type USING channel_type::text::channel_type;
+ALTER TABLE channels ALTER COLUMN channel_type SET DEFAULT 'TEXT';
+
+DROP TYPE channel_type_old;
+
+CREATE TABLE IF NOT EXISTS categories (
+    id BIGINT PRIMARY KEY,
+    sphere_id BIGINT NOT NULL,
+    name VARCHAR(32),
+    position INT,
+    FOREIGN KEY (sphere_id) REFERENCES spheres(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+ALTER TABLE channels ADD category_id BIGINT DEFAULT NULL REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE;
