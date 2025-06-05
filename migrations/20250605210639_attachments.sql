269618dcff9febc7eccfef8ac3a0d0c31eb25c36
GreenArron <GreenArronic@gmail.com>
2025-06-06 02:37:17 +0330

    fix(migrations): remove spoiler column from file

--- migrations/20250605210639_attachments.sql
+++ migrations/20250605210639_attachments.sql
@@ -5,3 +5,5 @@ ALTER TABLE message_attachments RENAME COLUMN attachment_id TO file_id;
 
 ALTER TABLE message_attachments DROP CONSTRAINT message_attachments_attachment_id_fkey;
 ALTER TABLE message_attachments ADD CONSTRAINT message_attachments_file_id_fkey FOREIGN KEY (file_id) REFERENCES files(id) ON DELETE CASCADE  ON UPDATE CASCADE;
+
+ALTER TABLE files DROP COLUMN spoiler;
