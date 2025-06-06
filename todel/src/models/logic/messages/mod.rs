463dd63a8319f3ab5ef773baab4194b5759091f3
GreenArron <GreenArronic@gmail.com>
2025-06-06 03:47:57 +0330

    fix: cargo fent

--- todel/src/models/logic/messages/mod.rs
+++ todel/src/models/logic/messages/mod.rs
@@ -109,16 +109,17 @@ impl Message {
         for (i, mut attachment_create) in message.attachments.into_iter().enumerate() {
             attachment_create.validate()?;
 
-            let attachment_file = match File::get(attachment_create.file_id, "attachments", db)
-                .await
-            {
-                Some(file) => file,
-                None => return Err(error!(
+            let attachment_file =
+                match File::get(attachment_create.file_id, "attachments", db).await {
+                    Some(file) => file,
+                    None => {
+                        return Err(error!(
                     VALIDATION,
                     format!("attachments-{}", i),
                     "Attachment's file must be a valid file that exists in the attachments bucket"
-                )),
-            };
+                ))
+                    }
+                };
 
             attachments.push(Attachment {
                 file: attachment_file.get_file_data(),
